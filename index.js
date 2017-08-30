b4cc2a4dbd99ae76a8995533e995975babc1f02c
David Keyworth <katananokage@gmail.com>
2017-08-29 21:18:06 -0400

    established schema

--- index.js
+++ index.js
@@ -6,11 +6,44 @@ var schema = new Schema('mysql', {
 	database: 'sentencemix'
 });
 
-var Wav_File = schema.define('Wav_File', {
+var WavFile = schema.define('WavFile', {
 	filename: {type: String, limit: 255},
-	uuid: {type: String, limit: 24}
-}, {
-	table: 'wav_file'
+	uuid: {type: String, limit: 24},
+	bitrate: {type: Number}
 });
 
+var LineGroup = schema.define('LineGroup', {
+	name: {type: String, limit: 255}
+});
+
+var Line = schema.define('Line', {
+	grammaticalSentence: {type: String, limit: 4096}
+});
+
+var Word = schema.define('Word', {
+	txt: {type: String, limit: 20},
+	begin: {type: Number, dataType: 'float', precision: 20},
+	end: {type: Number, dataType: 'float', precision: 20}
+});
+
+var CompoundWord = schema.define('CompoundWord', {
+	txt: {type: String, limit: 20}
+});
+
+var MixSegment = schema.define('MixSegment', {
+	begin: {type: Number, dataType: 'float', precision: 20},
+	end: {type: Number, dataType: 'float', precision: 20}
+});
+
+WavFile.belongsTo(Line);
+
+LineGroup.hasMany(Line);
+
+Line.hasMany(Word);
+
+WavFile.belongsTo(MixSegment);
+
+CompoundWord.hasMany(MixSegment);
+
 schema.automigrate();
+console.log("Automigration successful");
